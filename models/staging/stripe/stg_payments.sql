471de9d004fb5c19bf806ee902f640e25f630329
M Nandeesha <mnandeesha@deloitte.com>
2024-03-26 06:33:04 +0000

    commit

--- models/staging/stripe/stg_payments.sql
+++ models/staging/stripe/stg_payments.sql
@@ -1,4 +1,4 @@
-select
+{#select
     id as payment_id,
     orderid as order_id,
     paymentmethod as payment_method,
@@ -6,4 +6,15 @@ select
     -- amount is stored in cents, convert it to dollars
     amount / 100 as amount,
     created as created_at
-from {{ source('stripe', 'payment') }}
+from {{ source('stripe', 'payment') }} 
+--this is rebuild using Macros below
+#}
+select
+    id as payment_id,
+    orderid as order_id,
+    paymentmethod as payment_method,
+    status,
+    -- amount is stored in cents, convert it to dollars
+    {{ cents_to_dollars('amount', 4) }} as amount,
+    created as created_at
+from {{ source('stripe','payment') }}
